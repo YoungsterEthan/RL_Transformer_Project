465b9567ba22bf8da4697d829924191162713f3b
eorte052 <eorte052@ucr.edu>
2024-09-21 16:50:57 -0700

    Requirements.txt added and environment running

--- src/test.py
+++ src/test.py
@@ -1,8 +1,17 @@
 import gym
-import torch
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-env = gym.make('CartPole-v1')
-obs = env.reset()
+env = gym_super_mario_bros.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
 
-print("initial observation", obs)
-print("torch version: ", torch.__version__)
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = True
+for step in range(1000):
+    if done:
+        state = env.reset()
+    state, reward, done, truncates, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
